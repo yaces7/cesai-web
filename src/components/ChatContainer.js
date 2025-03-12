9a48248b8eb9e6cc4d9a66b035262ec8c463c224
Cesur Yasin <cesuryasin83@gmail.com>
2025-03-12 16:01:12 +0300

    first commit

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -230,7 +230,7 @@ const ChatContainer = () => {
     setIsTyping(true);
 
     try {
-      const response = await fetch('http://localhost:5000/chat', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/chat`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
